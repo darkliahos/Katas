dd7ae325b285712e739cac6f8d6903bccab7e143
Sohail Nasir <jtdinc2000@gmail.com>
2019-02-10 21:52:38 +0000

    First commit for Kata Tests

---
+++ c#/Kata.Main/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Main
+{
+    public static class Kata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            var result = string.Empty;
+            var lowercaseWord = word.ToLower();
+            foreach (var character in lowercaseWord)
+            {
+                var occurences = lowercaseWord.Count(c => c == character); //If this was an intensive operation then I would maybe not do this on each itteration
+                if (occurences > 1)
+                {
+                    result += ")";
+                }
+                else
+                {
+                    result += "(";
+                }
+                
+            }
+            return result;
+        }
+    }
+}
---
+++ c#/Kata.UnitTest/KataTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kata.Main;
+
+namespace Kata.UnitTest
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("(((", Main.Kata.DuplicateEncode("din"));
+            Assert.AreEqual("()()()", Main.Kata.DuplicateEncode("recede"));
+            Assert.AreEqual(")())())", Main.Kata.DuplicateEncode("Success"), "should ignorecase");
+            Assert.AreEqual("))((", Main.Kata.DuplicateEncode("(( @"));
+        }
+    }
+
+}
